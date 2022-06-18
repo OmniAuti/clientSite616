960cc701cd7e6b3db2e88fe294f95d718f69a95c
Austin Olijar <Austin.Olijar@gmail.com>
2022-06-17 19:29:54 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -23,3 +23,43 @@ downBtn.addEventListener('click', () => {
     var top = window.innerHeight;
     window.scrollTo(0,top);
 })
+
+const carouselControls = document.querySelectorAll('.carousel-control')
+const carousel = document.querySelector('.carousel')
+const controlArr = Array.from(carouselControls)
+carouselControls.forEach(control => {
+    control.addEventListener('click', () => {
+        var idx = controlArr.indexOf(control) * -50
+        carousel.style.transform = `translateX(${idx}%)`
+    })
+})
+
+const carouselControlsTwo = document.querySelectorAll('.carousel-control-two')
+const carouselTwo = document.querySelector('.carousel-two')
+const controlArrTwo = Array.from(carouselControlsTwo)
+carouselControlsTwo.forEach(control => {
+    control.addEventListener('click', () => {
+        var idx = controlArrTwo.indexOf(control) * -50
+        carouselTwo.style.transform = `translateX(${idx}%)`
+    })
+})
+
+const carouselArrows = document.querySelectorAll('.carousel-arrow')
+const controlArrowArr = Array.from(carouselArrows)
+console.log(carouselArrows, controlArrowArr)
+carouselArrows.forEach(control => {
+    control.addEventListener('click', () => {
+        
+        var idx = controlArrowArr.indexOf(control) * -50
+        carousel.style.transform = `translateX(${idx}%)`
+    })
+})
+const carouselArrowsTwo = document.querySelectorAll('.carousel-arrow-two')
+const controlArrowArrTwo = Array.from(carouselArrowsTwo)
+console.log(carouselArrows, controlArrowArr)
+carouselArrowsTwo.forEach(control => {
+    control.addEventListener('click', () => {
+        var idx = controlArrowArrTwo.indexOf(control) * -50
+        carouselTwo.style.transform = `translateX(${idx}%)`
+    })
+})
